2e864b5a467328757df4f5ca2384f1a90752385c
Lucas Lima Fernandes <lucas.lfernandes@live.com>
2024-07-30 16:46:16 -0300

    py version

---
+++ 2023/day03/part1/j.py
@@ -0,0 +1,109 @@
+def readfile(nome_arq):
+	with open(nome_arq, "r") as f:
+		linhas = f.readlines()
+	return linhas
+
+def monntaNro(i, j):	
+	jini = j
+	str_nro = ''	
+	while j<n_colunas and mat[i][j].isnumeric():				
+		str_nro += str(mat[i][j])		
+		mat[i][j] = '.'
+		j+=1
+	j = jini-1
+	while mat[i][j].isnumeric() and j>=0:
+		str_nro = str(mat[i][j]) + str_nro
+		mat[i][j] = '.'
+		j-=1	
+	return int(str_nro)
+
+def day_3_part1():
+
+	symbols = ""	
+	i=0
+	j=0
+	tot=0
+	for linha in linhas:		
+		j=0
+		for c in linha:
+			if not (c.isnumeric()) and c !="." and c not in symbols:
+				symbols +=c
+			if j<n_colunas and c !='\n':						
+				mat[i][j] = c				
+				j+=1
+		i+=1
+
+	print(symbols)
+
+	for i in range(0,n_linhas):
+		for j in range(0,n_colunas):
+			if mat[i][j] in symbols:				
+				if mat[i-1][j].isnumeric():					
+					tot+= monntaNro(i-1,j)
+				if mat[i-1][j+1].isnumeric():					
+					tot+= monntaNro(i-1,j+1)
+				if mat[i-1][j-1].isnumeric():											
+					tot+= monntaNro(i-1,j-1)									
+				if mat[i+1][j].isnumeric():						
+					tot+= monntaNro(i+1,j)
+				if mat[i+1][j+1].isnumeric():									
+					tot+= monntaNro(i+1,j+1)
+				if mat[i+1][j-1].isnumeric():					
+					tot+= monntaNro(i+1,j-1)						
+				if j-1 >= 0 and mat[i][j-1].isnumeric():					
+					tot+= monntaNro(i,j-1)				
+				if j+1 < n_colunas and mat[i][j+1].isnumeric():				
+					tot+= monntaNro(i,j+1)				
+	print(tot)
+
+def day_3_part2():
+
+	symbols = ""	
+	i=0
+	j=0
+	tot=0
+	for linha in linhas:		
+		j=0
+		for c in linha:
+			if not (c.isnumeric()) and c !="." and c not in symbols:
+				symbols +=c
+			if j<n_colunas and c !='\n':						
+				mat[i][j] = c				
+				j+=1
+		i+=1
+
+	for i in range(0,n_linhas):
+		for j in range(0,n_colunas):
+			if mat[i][j] == '*':				
+				nros = []				
+				if mat[i-1][j].isnumeric():					
+					nros.append(monntaNro(i-1,j))					
+				if mat[i-1][j+1].isnumeric():					
+					nros.append(monntaNro(i-1,j+1))					
+				if mat[i-1][j-1].isnumeric():		
+					nros.append(monntaNro(i-1,j-1))														
+				if mat[i+1][j].isnumeric():						
+					nros.append(monntaNro(i+1,j))					
+				if mat[i+1][j+1].isnumeric():	
+					nros.append(monntaNro(i+1,j+1))					
+				if mat[i+1][j-1].isnumeric():		
+					nros.append(monntaNro(i+1,j-1))					
+				if j-1 >= 0 and mat[i][j-1].isnumeric():					
+					nros.append(monntaNro(i,j-1))									
+				if j+1 < n_colunas and mat[i][j+1].isnumeric():	
+					nros.append(monntaNro(i,j+1))
+				if len(nros) == 2:
+					tot += nros[0] * nros[1]
+	print(tot)
+
+linhas = readfile("day3_input.txt")			
+n_colunas = len(linhas)
+n_linhas = len(linhas)
+mat = [[""]*n_colunas for _ in range(n_linhas)]
+day_3_part1()
+
+linhas = readfile("day3_input.txt")
+n_colunas = len(linhas)
+n_linhas = len(linhas)
+mat = [[""]*n_colunas for _ in range(n_linhas)]
+day_3_part2()
